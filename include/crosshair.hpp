7f172459f1f2da374dbb27edbb55534b2377b9af
jc <jc>
2024-05-14 07:14:45 -0500

    doafja

--- include/crosshair.hpp
+++ include/crosshair.hpp
@@ -1,25 +1,29 @@
 #ifndef CROSSHAIR_HPP
 #define CROSSHAIR_HPP
 
-// Class to draw a crosshair on a ROS image
-class Crosshair
-{
-    public:
-        int x_crosshair;
-        int y_crosshair;
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <sensor_msgs/Image.h>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
 
-        // Constructor
-        explicit Crosshair(void);
+class Crosshair {
+public:
+    Crosshair(ros::NodeHandle& nh);
+    ~Crosshair();
 
-        // Destructor
-        ~Crosshair(void);
+private:
+    ros::NodeHandle _nh;
+    image_transport::ImageTransport _it;
+    image_transport::Subscriber _image_sub;
+    image_transport::Publisher _image_pub;
 
+    cv::Mat _crosshair_image;
 
-    private:
-
-        void _draw_crosshair(void);
-
+    void _imageCallback(const sensor_msgs::ImageConstPtr& msg);
+    void _loadCrosshairImage();
 };
 
-
-#endif
+#endif // CROSSHAIR_HPP
--- src/crosshair.cpp
+++ src/crosshair.cpp
@@ -3,8 +3,8 @@
 // TODO: 
 // 1. Subscribe to a ROS topic sensor_msgs/Image
 // 2. Draw a crosshair on the image
-// 3. Publish the crosshair image to a new ROS topic sensor_msgs/Image and the crosshair center position to a new ROS topic geometry_msgs/Point
-Crosshair::Crosshair()
+// 3. Publish the crosshair image to a new ROS topic sensor_msgs/Image and the crosshair center position to a new ROS topic geometry_msgs/Point 
+Crosshair::Crosshair(ros::NodeHandle& nh) : _nh(nh), _it(_nh) 
 {
 
 }
